d5708a988b2b6cc2a58de87ee7758c1c60a6a22f
Liz R <lizr588@gmail.com>
2018-07-20 10:24:57 -0500

    Friday

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  post 'add_to_cart' => 'cart#add_to_cart'
+  get 'view_order' => 'cart#view_order'
+  get 'checkout' => 'cart#checkout'
+  root 'storefront#all_items'
+  get 'categorical' => 'storefront#items_by_category'
+  get 'branding' => 'storefront#items_by_brand'
+  # root 'products#index'
+
+  devise_for :users
+  resources :products
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
